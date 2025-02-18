174726f2c1f07cca61b81bff46becc8cbb2bf190
Jubril <jubfeb20@gmail.com>
2025-02-18 18:19:00 +0100

    adding the refund function incase goal is'nt met

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -13,8 +13,9 @@ contract CrowdFunding {
 
     event cancel(uint _id);
     event pledge(uint256 _id, address _address, uint256 _amount);
-    event unPledge(uint256 _id, address indexed _address, uint _amount);
-    event claim(uint256 _id);
+    event UnPledge(uint256 _id, address indexed _address, uint _amount);
+    event Claimed(uint256 _id);
+    event Refund(uint256 indexed _id, address indexed _address, uint256 _amount);
 
     struct Campaign {
         address campaignOwner;
@@ -72,25 +73,36 @@ contract CrowdFunding {
         emit pledge(_id, msg.sender, _amount);
     }
 
-    function UnPledge(uint256 _id, uint256 _amount) external {
+    function unPledge(uint256 _id, uint256 _amount) external {
         Campaign storage campaign = Campaigns[_id];
         require(block.timestamp > campaign.startAt, "Not started");
         require(block.timestamp < campaign.endAt, "Ended");
         campaign.pledge -= _amount;
         pledgedAmount[_id][msg.sender] -= _amount;
 
-        emit unPledge(_id, msg.sender, _amount);
+        emit UnPledge(_id, msg.sender, _amount);
     }
 
-    function Claimed(uint256 _id) external {
+    function claim(uint256 _id) external {
         Campaign storage campaign = Campaigns[_id];
-        require(campaign.campaignOwner == msg.sender);
-        require(block.timestamp >= campaign.endAt, "ongoing campaign");
-        require(campaign.pledge >= campaign.goal, "goal not reached");
+        require(msg.sender == campaign.campaignOwner, "must be owner");
+        require(campaign.pledge >= campaign.goal, "not fulfilled");
         require(!campaign.claimed, "claimed");
 
         campaign.claimed = true;
         token.transfer(msg.sender, campaign.pledge);
-        emit claim(_id);
+        emit Claimed(_id);
     }
+
+    function refund(uint256 _id) external{
+        Campaign storage campaign =  Campaigns[_id];
+        require(block.timestamp > campaign.endAt);
+        require(campaign.goal > campaign.pledge,"goal achieved");
+
+        uint256 bal = pledgedAmount[_id][msg.sender];
+        pledgedAmount[_id][msg.sender] = 0;
+        token.transfer(msg.sender, bal);
+
+        emit Refund()
+    } 
 }
